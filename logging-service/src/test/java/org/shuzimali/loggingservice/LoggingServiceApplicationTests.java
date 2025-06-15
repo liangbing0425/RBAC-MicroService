b642a7693a70ea466dce00140bea1d934b78b9de
lb <1604666464@qq.com>
2025-06-15 23:26:10 +0800

    创建项目

---
+++ logging-service/src/test/java/org/shuzimali/loggingservice/LoggingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.shuzimali.loggingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ permission-service/src/test/java/org/shuzimali/permissionservice/PermissionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.shuzimali.permissionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PermissionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
